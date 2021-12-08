718060979277f1fc43088a94ef90d88a1f186607
indra.kurniawan <indra.kurniawan@kasn.go.id>
2021-12-09 06:57:38 +0700

    ceri indikator menu active pada navbar

--- routes/web.php
+++ routes/web.php
@@ -20,12 +20,14 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home', [
-        "title" => "Home"
+        "title" => "Home",
+        "active" => "home"
     ]);
 });
 Route::get('/about', function () {
     return view('about',[
         "title" => "About",
+        "active" => "about",
         "name" => "Indra Kurniawan",
         "email" => "Mbasss.ind@gmail.com",
         "image" => "Indra_Kurniawan.png"
