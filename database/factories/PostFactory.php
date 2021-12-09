accd8b9e298dd29f7abe26591ba67579eb42bbfd
indra.kurniawan <indra.kurniawan@kasn.go.id>
2021-12-09 19:03:05 +0700

    penyesuaian pada factori opost, memberikan paragraph

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -16,8 +16,25 @@ class PostFactory extends Factory
         return [
             'title' => $this->faker->sentence(mt_rand(2,8)),
             'slug' => $this->faker->slug(),
-            'excerpt' => $this->faker->paragraph(mt_rand(5,10)), 
-            'body' => $this->faker->paragraph(mt_rand(20,100)),
+            'excerpt' => $this->faker->paragraph(), 
+
+            // Pembuatan paragraph pada array fakerParagraph 
+            // cara 1
+            // 'body' => '<p>' . implode('</p><p>', $this->faker->paragraphs(mt_rand(5, 10))). '</p>',
+
+            // cara 2 menggubnakan map
+            'body' => collect($this->faker->paragraphs(mt_rand(5, 10)))
+                            
+                            // map tanpa arrow function 
+                            // ->map(function($p) {
+                            //     return "<p>$p</p>";
+                            // })
+                            // ->implode(''),
+
+                            // menggunakan arrow function
+                            ->map(fn($p) => "<p>$p</p>")
+                            ->implode(''),
+            
             'category_id' => mt_rand(1,3),
             'user_id' => mt_rand(1,3),
         ];
